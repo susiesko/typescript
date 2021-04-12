4bbe83570266d15cb0c33fe1a068981002fc8b6f
Susie Rennick <susie.sko@gmail.com>
2021-04-11 19:28:14 -0700

    after lecture 124

---
+++ drag-and-drop-project/src/app.ts
@@ -0,0 +1,70 @@
+// autobind decorators
+function Autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+// ProjectInput Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInput: HTMLInputElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'project-input'
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
+
+    this.titleInputElement = this.element.querySelector(
+      '#title'
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      '#description'
+    ) as HTMLInputElement;
+    this.peopleInput = this.element.querySelector(
+      '#people'
+    ) as HTMLInputElement;
+
+    this.configure();
+    this.attach();
+  }
+
+  @Autobind
+  private submitHandler(event: Event) {
+    event.preventDefault();
+  }
+
+  private configure() {
+    this.element.addEventListener('submit', this.submitHandler);
+  }
+
+  private attach() {
+    this.hostElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+const prjInput = new ProjectInput();
